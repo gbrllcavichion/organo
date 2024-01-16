c7248d521c17bef0cb66e3d13d2f252513c1844d
gbrllcavichion <cavichionmichel@gmail.com>
2024-01-16 19:19:23 -0300

    ♻️ Refactoring code

--- src/App.js
+++ src/App.js
@@ -55,8 +55,15 @@ function App() {
     <div className="App">
       <Banner/>
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
-      <Time nome="Programação"/>
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)}   
+      
     </div>
   );
 }
