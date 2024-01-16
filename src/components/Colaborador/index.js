ae218bf42d523c96f2a4c713adb84f57573854d8
gbrllcavichion <cavichionmichel@gmail.com>
2024-01-16 20:07:38 -0300

    ✨ Creating collaborators and css

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'> 
+            <img src='https://github.com/gbrllcavichion.png' alt='Gabrielle Cavichion'/>
+        </div>
+            
+        <div className='rodape'>
+            <h4>Gabrielle Cavichion</h4>
+            <h5>Software Engineer I</h5>
+        </div>
+    </div>)
+} 
+
+export default Colaborador
