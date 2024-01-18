9ae316389a4b43d5ac92b53912fa9358a7eb4e13
Gabrielle Cavichion <102532618+gbrllcavichion@users.noreply.github.com>
2024-01-18 16:52:50 -0300

    :bug: Wasn't adding collaborators to team "Programação"

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -9,7 +9,7 @@ const Formulario = (props) => {
     const [nome, setNome] = useState(``)
     const [cargo, setCargo] = useState(``)
     const [imagem, setImagem] = useState(``)
-    const [time, setTime] = useState(``)
+    const [time, setTime] = useState(`Programação`)
 
 
     const aoSalvar = (evento) => {
