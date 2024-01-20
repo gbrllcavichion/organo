114ef6e8f7dc7b5e67a627a2de0c76031827cffa
gbrllcavichion <cavichionmichel@gmail.com>
2024-01-19 23:12:13 -0300

    ✨ Creating footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/Formulario';
 import Time from './components/Time'
+import Rodape from './components/Rodape';
 
 function App() {
 
@@ -64,6 +65,8 @@ function App() {
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}   
 
+      <Rodape/>
+      
     </div>
   );
 }
