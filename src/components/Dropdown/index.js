655bf307395de393036a3b95d5c501426ba7966f
gbrllcavichion <cavichionmichel@gmail.com>
2024-01-22 22:48:38 -0300

    💄 Upgrading organo due teacher changes...

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,17 +1,13 @@
 import './Dropdown.css'
 
-const Dropdown = (props) => {
-    return (
-        <div className='dropdown'>
-            <label>
-                {props.label}
-            </label>
-            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
-                <option value=""></option>
-                {props.itens.map(item => <option key={item}>{item} </option>)}
-            </select>
-        </div>
-    )
+const ListaSuspensa = ({label, items,  valor, aoAlterado, obrigatorio = false}) => {
+    return (<div className="dropdown">
+        <label>{label}</label>
+        <select required={obrigatorio} value={valor} onChange={evento => aoAlterado(evento.target.value)}>
+            <option />
+            {items.map(item => <option key={item}>{item}</option>)}
+        </select>
+    </div>)
 }
 
-export default Dropdown
+export default ListaSuspensa
